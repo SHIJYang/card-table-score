5b5e45b31c14e28d743fed5162ec82f8ebb2b7e2
SHIJYang <1457794641@qq.com>
2025-03-18 18:16:31 +0800

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function updateScore(playerId, change) {
-    const playerInput = document.getElementById(playerId);
-    let currentScore = parseInt(playerInput.value, 10);
-    currentScore += change;
-    playerInput.value = currentScore;
-}
