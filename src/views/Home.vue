598b2f6464fd1f8a735ef839e80e24d2880ab833
SHIJYang <1457794641@qq.com>
2025-10-29 11:01:47 +0800

    1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,40 +2,57 @@
   <div class="common-layout">
     <topnav />
 
-    <div class="bg">
-      <!-- <Squares
-        direction="diagonal"
-        :speed="0.5"
-        :squareSize="20"
-        borderColor="#999"
-        hoverFillColor="#222"
-        style="height: 100%; width: 100%"
-      /> -->
-
-      <ClickSpark
-        spark-color="#0dffb7"
-        :spark-size="10"
-        :spark-radius="40"
-        :spark-count="20"
-        :duration="600"
-        easing="ease-out"
-        :extra-scale="1.2"
-        class="interactive-area"
-      ></ClickSpark>
+    <div class="main">
+      <el-card style="width: 80%; height: 200px"
+        ><CircularGallery
+          :items="[
+            {
+              image: 'https://picsum.photos/800/600?random=1',
+              text: 'Beautiful Nature',
+            },
+            {
+              image: 'https://picsum.photos/800/600?random=2',
+              text: 'City Life',
+            },
+            {
+              image: 'https://picsum.photos/800/600?random=3',
+              text: 'Ocean Views',
+            },
+          ]"
+          :bend="0"
+          text-color="#afafaf"
+          :border-radius="0.05"
+          font="bold 30px Arial"
+          :scroll-speed="5"
+          :scroll-ease="0.05"
+          class="canvas"
+      /></el-card>
+      <div>
+        <ShinyText
+          text="Just some shiny text!"
+          :disabled="false"
+          :speed="3"
+          class-name="your-custom-class"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import Topnav from "../views/topnav/TopNav.vue";
-import ClickSpark from "../components/ClickSpark.vue";
-import Squares from "../components/gsap/Squares.vue";
+
+import CircularGallery from "../components/gsap/CircularGallery.vue";
+import ShinyText from "../components/gsap/ShinyText.vue";
 </script>
 
 <style scoped>
-.bg {
+.main {
   width: 100%;
-  height: 100vh;
-  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
 }
 </style>
--- src/views/games/try.vue
+++ src/views/games/try.vue
@@ -26,10 +26,50 @@
     </el-container>
   </div>
 </template>
+<!-- <TiltedCard
+        image-src="https://picsum.photos/800/600?random=3"
+        alt-text="Sample image"
+        caption-text="Hover to see tooltip"
+        container-height="200px"
+        container-width="200px"
+        image-height="200px"
+        image-width="200px"
+        :rotate-amplitude="19"
+        :scale-on-hover="1.1"
+        :show-mobile-warning="true"
+        :show-tooltip="true"
+        :display-overlay-content="false"
+      >
+        <template #overlay>
+          <div class="overlay-content">Your overlay content here</div>
+        </template>
+      </TiltedCard> -->
+<!-- <Squares
+        direction="diagonal"
+        :speed="0.5"
+        :squareSize="20"
+        borderColor="#999"
+        hoverFillColor="#222"
+        style="height: 100%; width: 100%"
+      /> -->
+
+<!-- <ClickSpark
+        spark-color="#0dffb7"
+        :spark-size="10"
+        :spark-radius="40"
+        :spark-count="20"
+        :duration="600"
+        easing="ease-out"
+        :extra-scale="1.2"
+        class="interactive-area"
+      ></ClickSpark> -->
 
 <script setup>
 import Topnav from "../topnav/TopNav.vue";
 import FuzzyText from "../../components/gsap/FuzzyText.vue";
+import ClickSpark from "../../components/ClickSpark.vue";
+import Squares from "../../components/gsap/Squares.vue";
+import TiltedCard from "../../components/gsap/TiltedCard.vue";
 </script>
 
 <style scoped>
